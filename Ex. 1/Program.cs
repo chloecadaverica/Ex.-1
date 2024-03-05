b23e15fc551e1b58e37c62bbf7bf067f9690616b
chloe <chloe@DESKTOP-MHQ5M5I>
2024-03-05 19:37:12 -0300

    Adicionar arquivos de projeto.

---
+++ Ex. 1/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] sal = new double[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Digite o {i + 1}º salário: ");
+    sal[i] = double.Parse(Console.ReadLine());
+}
+
+double maiorSal = sal[0];
+
+for (int i = 1; i < 10; i++)
+{
+    if (sal[i] > maiorSal)
+    {
+        maiorSal = sal[i];
+    }
+}
+Console.WriteLine($"O maior salário é: {maiorSal}");
